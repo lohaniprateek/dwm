2a2ed1dd2ef617589d5fd69e77160cebc3e1ac36
Prateek Kumar <hlw.prateek@gmail.com>
2026-03-15 11:45:29 +0530

    Added: Xresource patch and gitigonre for orig and rej files

--- config.h
+++ config.h
@@ -1,24 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx        = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /*gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int snap            = 32;       /* snap pixel */
+static int showbar                  = 1;        /* 0 means no bar */
+static int topbar                   = 1;        /* 0 means bottom bar */
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int systrayonleft      = 0;        /* 0 means right, >0 means left */
 static const int systrayspacing     = 2;        /* systray spacing */
 static const int systraypinning     = 0;        /* 0 means follow selected monitor */
 static const int systraypinningfailfirst = 1;   /* 1: pin systray to first monitor on fail */
 static const int user_bh            = 0;
-static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=14", "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static char font[]                  = "JetBrainsMono Nerd Font:size=14";
+static char dmenufont[]             = "monospace:size=12";
+static const char *fonts[]          = { font, "monospace:size=12" };
+static char normbgcolor[]           = "#282828";
+static char normbordercolor[]       = "#504945";
+static char normfgcolor[]           = "#ebdbb2";
+static char selfgcolor[]            = "#ebdbb2";
+static char selbordercolor[]        = "#d79921";
+static char selbgcolor[]            = "#3c3836";
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
@@ -29,8 +31,8 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor, selbordercolor },
 };
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4","5","6" };
@@ -46,9 +48,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static float mfact           = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster           = 1;    /* number of clients in master area */
+static int resizehints       = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
@@ -72,10 +74,28 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brightup[] = {"brightnessctl", "set", "+10%", NULL};
 static const char *brightdown[] = {"brightnessctl", "set", "10%-", NULL};
+
+ResourcePref resources[] = {
+	{ "font", STRING, &font },
+	{ "dmenufont", STRING, &dmenufont },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "normbordercolor", STRING, &normbordercolor },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "selbgcolor", STRING, &selbgcolor },
+	{ "selbordercolor", STRING, &selbordercolor },
+	{ "selfgcolor", STRING, &selfgcolor },
+	{ "borderpx", INTEGER, &borderpx },
+	{ "snap", INTEGER, &snap },
+	{ "showbar", INTEGER, &showbar },
+	{ "topbar", INTEGER, &topbar },
+	{ "nmaster", INTEGER, &nmaster },
+	{ "resizehints", INTEGER, &resizehints },
+	{ "mfact", FLOAT, &mfact },
+};
 
 static const Key keys[] = {
 	/* modifier                     key                                        function        argument */
